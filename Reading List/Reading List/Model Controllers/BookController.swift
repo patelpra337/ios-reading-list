055f7d5c82e944dbd6f320cc219d7f6cc98fed1b
Pravin Patel <patelpra337@gmail.com>
2019-12-07 13:02:05 -0600

    Update storyboard and bookController

--- Reading List/Reading List/Model Controllers/BookController.swift
+++ Reading List/Reading List/Model Controllers/BookController.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+class BookController {
+    
+    // MARK: - Books empty array
+    private(set) var books: [Book] = []
+    
+    // MARK: - Computed property
+    private var readingListFileURL: URL? {
+        
+        let fileManager = FileManager.default
+        guard let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        
+        return documentDirectory.appendingPathComponent("ReadingList.plist")
+    }
+    
+    // MARK: - CRUD (Create, Read, Update, Delete)
+    
+    
+    
+    // MARK: - Save to disk
+    func saveToPersistentStore() {
+        guard let url = self.readingListFileURL else { return }
+        
+        do {
+            let encoder = PropertyListEncoder()
+            let booksData = try encoder.encode(books)
+            try booksData.write(to: url)
+        } catch {
+            print("Error saving books data: \(error)")
+        }
+    }
+    
+    // MARK: - Load from disk
+    func loadFromPersistentStore() {
+        let fileManager = FileManager.default
+        guard let url = readingListFileURL, fileManager.fileExists(atPath: url.path) else { return }
+        
+        do {
+            let booksData = try Data(contentsOf: url)
+            let decodedBooks = PropertyListDecoder()
+            self.books = try decodedBooks.decode([Book].self, from: booksData)
+        } catch {
+            print("Error loading books data: \(error)")
+        }
+    }
+}
+
+
