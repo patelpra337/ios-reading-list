5df61d9ac727c05d90e0a1a4cc0a5f9d03c40a92
Pravin Patel <patelpra337@gmail.com>
2019-12-07 10:54:28 -0600

    Create storyboard, layout, model files and view controllers

---
+++ Reading List/Reading List/Model Controllers/BookController.swift
@@ -0,0 +1,9 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by patelpra on 12/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Reading List/Reading List/Model Controllers/BookTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by patelpra on 12/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+
+
+}
